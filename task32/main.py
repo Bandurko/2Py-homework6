38a7b25f2740444fa6942a44c2f8d5e4670e3753
“Sergey” <s22-01-15@yandex.ru>
2023-04-28 08:41:54 +1000

    task32

---
+++ task32/main.py
@@ -0,0 +1,29 @@
+# Задача 32: Определить индексы элементов массива (списка), 
+# значения которых принадлежат заданному диапазону (т.е. не 
+# меньше заданного минимума и не больше заданного максимума)
+
+# Input:
+# 1, 3, 7, 10, 5, 8
+# 4
+# 8
+# Output:
+# 2(7), 4(5), 5(8)
+
+def index(nums, min, max):
+    list = []
+    for i in range(len(nums)):
+        if nums[i] <= max and nums[i] >= min:
+            list.append(i)
+    return list
+
+from random import randint
+
+lenght_nums = int(input('Введите длину массива (списка): '))
+nums = list(randint(1, 10) for i in range(lenght_nums))
+
+print(nums)
+
+min = int (input('Введите минимальное значение заданного диапазона: '))
+max = int (input('Введите максимальное значение заданного диапазона: '))
+
+print(index(nums, min, max))
